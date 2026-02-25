bda9121d85f6bdc37cd636e0dd65e9105e09b7af
Suraj Chaudhary <isurajcdry@gmail.com>
2026-02-25 15:08:58 -0500

    added contribute redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/dev",
+        destination: "https://github.com/surajcdry/sjnycsclub",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
