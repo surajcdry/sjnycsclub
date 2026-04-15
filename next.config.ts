d028a2b1408c866133940ac11b5a828b33d9a0c7
Suraj Chaudhary <isurajcdry@gmail.com>
2026-04-15 14:12:17 -0400

    added event attendance link

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         destination: "https://github.com/surajcdry/sjnycsclub",
         permanent: true,
       },
+      {
+        source: "/attend",
+        destination: "https://docs.google.com/forms/d/e/1FAIpQLSdAl4mt_tWROGhUNxKm_PDdHz5bTYU5K5rK4LVDn7o3OZHKSA/viewform",
+        permanent: true,
+      },
     ];
   },
 };
